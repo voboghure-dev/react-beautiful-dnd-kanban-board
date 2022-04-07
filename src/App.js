f418d111763ddc15b14c665d00f1c2987c5ac1f4
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-04-07 15:14:39 +0600

    Rendering movable item (half complete)
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,96 @@
+import { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { v4 as uuid } from 'uuid';
+
+const itemsFromBackend = [
+  { id: uuid(), content: 'First task' },
+  { id: uuid(), content: 'Second task' },
+  { id: uuid(), content: 'Third task' },
+  { id: uuid(), content: 'Fourth task' },
+  { id: uuid(), content: 'Fifth task' },
+];
+
+const columnsFromBackend = {
+  [uuid()]: {
+    name: 'Requested',
+    items: itemsFromBackend,
+  },
+  [uuid()]: {
+    name: 'To do',
+    items: [],
+  },
+  [uuid()]: {
+    name: 'In Progress',
+    items: [],
+  },
+  [uuid()]: {
+    name: 'Done',
+    items: [],
+  },
+};
+
 function App() {
+  const [columns, setColumns] = useState(columnsFromBackend);
   return (
-    <div className='App'>
-      Main App
+    <div style={{ display: 'flex', justifyContent: 'center', height: '100%' }}>
+      <DragDropContext onDragEnd={(result) => console.log(result)}>
+        {Object.entries(columns).map(([id, column]) => {
+          return (
+            <Droppable droppableId={id} key={id}>
+              {(provided, snapshot) => {
+                return (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={{
+                      background: snapshot.isDraggingOver
+                        ? 'lightblue'
+                        : 'lightgray',
+                      padding: 4,
+                      width: 250,
+                      minHeight: 500,
+                    }}
+                  >
+                    {column.items.map((item, index) => {
+                      return (
+                        <Draggable
+                          key={item.id}
+                          draggableId={item.id}
+                          index={index}
+                        >
+                          {(provided, snapshot) => {
+                            return (
+                              <div
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                style={{
+                                  userSelect: 'none',
+                                  padding: 16,
+                                  margin: '0 0 8px 0',
+                                  minHeight: '50px',
+                                  backgroundColor: snapshot.isDragging
+                                    ? '#263B4A'
+                                    : '#456C86',
+                                  color: 'white',
+                                  ...provided.draggableProps.style,
+                                }}
+                              >
+                                {item.content}
+                              </div>
+                            );
+                          }}
+                        </Draggable>
+                      );
+                    })}
+                    {provided.placeholder}
+                  </div>
+                );
+              }}
+            </Droppable>
+          );
+        })}
+      </DragDropContext>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
